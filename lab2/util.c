bf466eb7f773420eba2d37d3f96079970aec2f5a
Tu Tran <tu.tran@temple.edu>
2017-03-07 02:29:08 -0500

    fix implicit declaration

--- lab2/util.c
+++ lab2/util.c
@@ -16,14 +16,18 @@
  * =====================================================================================
  */
 
+#define _GNU_SOURCE
+#define _XOPEN_SOURCE
+
+#include <dirent.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <string.h>
-#include <dirent.h>
+#include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "shell.h"
 
 
